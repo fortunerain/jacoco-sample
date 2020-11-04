e2e45d49573b858bf808bcef7e0f05d897eb1e2d
nomino <fortunerain@naver.com>
2020-11-04 09:36:51 +0900

    init

---
+++ src/main/java/com/jacoco/sample/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jacoco.sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @GetMapping(value = "/home")
+  public String home() {
+    return "home";
+  }
+}
---
+++ src/test/java/com/jacoco/sample/controller/HomeControllerTest.java
@@ -0,0 +1,31 @@
+package com.jacoco.sample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HomeControllerTest {
+
+  @Autowired
+  private TestRestTemplate testRestTemplate;
+
+  @LocalServerPort
+  private int port;
+
+  @Test
+  public void home() {
+    String result = testRestTemplate.getForObject("/home", String.class);
+
+    assertThat(result).isEqualTo("home");
+
+
+  }
+}
